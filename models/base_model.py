b4d1d5e6c23b44bd1de5977d74c0ea3a88706abb
santaETH <sdmissie@yhoo.com>
2024-02-13 05:38:07 -0800

    AirBnB Clone, the Console project

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
 
+import uuid
+import models
+from datetime import datetime
 
 class BaseModel:
 	"""A base class for all hbnb models"""
