d86a9769faef7e7676e5065f9eb85ea09924fb64
santaETH <sdmissie@yhoo.com>
2024-02-13 06:21:35 -0800

    AirBnB Clone, the Console project

--- console.py
+++ console.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" This module Implementing for console AirBnB project """
-
+"""Defines the HBnB console."""
 import cmd
-import json
 import re
-import shlex import split
-from datetime import datetime
+from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
 from models.place import Place
-from models.review import Review
 from models.amenity import Amenity
+from models.review import Review
+
 
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
